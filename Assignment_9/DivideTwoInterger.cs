ac1f9298a2674f565d16392eada2f6f7bcb90e2d
ABHISHEK\jilla <ajilla@avidxchange.com>
2022-10-30 23:59:48 +0530

    Assignment 9

--- Assignment_9/DivideTwoInterger.cs
+++ Assignment_9/DivideTwoInterger.cs
@@ -17,47 +17,16 @@ namespace DSA.Assignment_9
         public void Run()
         {
             //Diver Code
-            int dividend = 123456;//2147483647; //-2147483648;
-            int divisor = -3;
+            int dividend = 125;
+            int divisor = 5;
 
-            //var result = CalculateDivision(dividend, divisor);
             var result = Find(dividend, divisor);
             Console.WriteLine($"Division of {dividend} by {divisor} is: " + result);
         }
 
         public int Find(int dividend, int divisor)
         {
-            //if (dividend == int.MinValue && divisor == -1) return int.MaxValue; 
-            //Cornor case when -2^31 is divided by -1 will give 2^31 which doesnt exist so overflow 
-
-            //bool negative = dividend < 0 ^ divisor < 0; 
-            //Logical XOR will help in deciding if the results is negative only if any one of them is negative
-
             int sign = divisor > 0 == dividend > 0 ? 1 : -1;
-
-            //Covering the Constraints
-            if (dividend > int.MaxValue)
-                return sign * int.MaxValue - 1;
-
-            if (dividend < int.MinValue)
-                return sign * int.MinValue;
-
-            if (dividend == int.MinValue && (divisor == -1 || divisor == 1))
-            {
-                if (divisor == -1)
-                    return sign * int.MinValue - 1;
-
-                return sign * int.MinValue;
-            }
-
-            if (dividend == int.MaxValue && (divisor == -1 || divisor == 1))
-                return sign * int.MaxValue;
-
-            if (divisor == 1)
-                return dividend;
-
-            if (divisor == -1)
-                return -(dividend);
 
             //Converting the negative number to positive
             dividend = Math.Abs(dividend);
@@ -74,65 +43,11 @@ namespace DSA.Assignment_9
                 quotient += 1 << subQuot;
 
                 //Substract from dividend to start over with the remaining
-                dividend -= divisor << subQuot; 
+                dividend -= divisor << subQuot;
             }
 
             return quotient * sign;
         }
 
-        public int CalculateDivision(int dividend, int divisor)
-        {
-            int sign = divisor > 0 == dividend > 0 ? 1 : -1;
-
-            if (dividend > int.MaxValue)
-                return sign * int.MaxValue - 1;
-
-            if (dividend < int.MinValue)
-                return sign * int.MinValue;
-
-            if (dividend == int.MinValue && (divisor < 0 || divisor == 1))
-            {
-                if (divisor == -1)
-                    return sign * int.MinValue - 1;
-
-                return sign * int.MinValue;
-            }
-
-            if (dividend == int.MaxValue && (divisor < 0 || divisor == 1))
-                return sign * int.MaxValue;
-
-            if (divisor == 1)
-                return dividend;
-
-            if (divisor == -1)
-                return -(dividend);
-
-            int _dividend = Math.Abs(dividend);
-            int _divisor = Math.Abs(divisor);
-
-            int quotient = 0;
-            double currentDivisor = 0;
-
-            while (currentDivisor <= _dividend)
-            {
-                currentDivisor += _divisor;
-
-                if (currentDivisor == _dividend)
-                {
-                    quotient++;
-                    return sign * quotient;
-                }
-                else if (currentDivisor < _dividend)
-                {
-                    quotient++;
-                }
-                else if (currentDivisor > _dividend)
-                {
-                    return sign * quotient;
-                }
-            }
-
-            return sign * quotient;
-        }
     }
 }
--- Assignment_9/FindMedianOfSortedTwoArray.cs
+++ Assignment_9/FindMedianOfSortedTwoArray.cs
@@ -17,25 +17,91 @@ namespace DSA.Assignment_9
     {
         public double Run()
         {
-            int[] nums1 = new int[2] { 1, 3 };
-            int[] nums2 = new int[1] { 2 };
+            int[] nums1 = new int[4] { 1, 3, 4, 5 };
+            int[] nums2 = new int[4] { 2, 7, 9, 10 };
 
-            var mergedArray = MergeTwoSortedArray(nums1, nums2);
+            double medianValue = FindMedian(nums1, nums2);
 
-            double medianValue;
-            var halfLength = mergedArray.Length / 2;
-            if (mergedArray.Length % 2 != 0)
-            {
-                medianValue = mergedArray[halfLength];
-            }
-            else
-            {
-                medianValue = ((double)mergedArray[halfLength] + (double)mergedArray[halfLength - 1]) / 2;
-            }
+            //var mergedArray = MergeTwoSortedArray(nums1, nums2);
+
+
+            //var halfLength = mergedArray.Length / 2;
+            //if (mergedArray.Length % 2 != 0)
+            //{
+            //    medianValue = mergedArray[halfLength];
+            //}
+            //else
+            //{
+            //    medianValue = ((double)mergedArray[halfLength] + (double)mergedArray[halfLength - 1]) / 2;
+            //}
 
             Console.WriteLine($"Median: {medianValue}");
             return medianValue;
         }
+
+
+        public double FindMedian(int[] nums1, int[] nums2)
+        {
+            // Check if num1 is smaller than num2
+            // If not, then we will swap num1 with num2
+            if (nums1.Length > nums2.Length)
+                return FindMedian(nums2, nums1);
+
+            // Lengths of two arrays
+            int m = nums1.Length;
+            int n = nums2.Length;
+
+            //pointer for binarySearch
+            int start = 0;
+            int end = m;
+
+            while (start <= end)
+            {
+                //partition 
+                int partitionNums1 = (start + end) / 2;
+                int partitionNums2 = (n + m + 1) / 2 - partitionNums1;
+
+                // Edge cases
+
+                // If there are no elements left on the left side after partition
+                int maxLeftNums1 = partitionNums1 == 0 ? int.MinValue : nums1[partitionNums1 - 1];
+                // If there are no elements left on the right side after partition
+                int minRightNums1 = partitionNums1 == m ? int.MaxValue : nums1[partitionNums1];
+
+                // If there are no elements left on the left side after partition
+                int maxLeftNums2 = partitionNums2 == 0 ? int.MinValue : nums2[partitionNums2 - 1];
+                // If there are no elements left on the right side after partition
+                int minRightNums2 = partitionNums2 == n ? int.MaxValue : nums2[partitionNums2];
+
+                // Check if we have found the match
+                if (maxLeftNums1 <= minRightNums2 && maxLeftNums2 <= minRightNums1)
+                {
+                    // Check if the combined array is of even/odd length
+                    if ((m + n) % 2 == 0)
+                    {
+                        return (Math.Max(maxLeftNums1, maxLeftNums2) + Math.Min(minRightNums1, minRightNums2)) / 2.0;
+                    }
+                    else
+                    {
+                        return Math.Max(maxLeftNums1, maxLeftNums2);
+                    }
+                }
+                // If we are too far on the right, we need to go to left side
+                else if (maxLeftNums1 > minRightNums2)
+                {
+                    end = partitionNums1 - 1;
+                }
+                // If we are too far on the left, we need to go to right side
+                else
+                {
+                    start = partitionNums1 + 1;
+                }
+            }
+
+            return -1;
+
+        }
+
 
         //Time Complexity: O(m+n)
         public int[] MergeTwoSortedArray(int[] firstArr, int[] secondArr)
@@ -144,7 +210,7 @@ namespace DSA.Assignment_9
                     var nums1Index = i1 + 1;
 
                     //we discard the first half of nums1
-                    return FindMedianElement(nums1[nums1Index..], nums2, k - i1 - 1);                    
+                    return FindMedianElement(nums1[nums1Index..], nums2, k - i1 - 1);
                 }
                 else
                 {
